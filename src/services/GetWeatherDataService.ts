8d1c9821f5ae9963156e5db26332a60d15bf6d2d
thtomazella <th.tomazella@gmail.com>
2022-06-07 17:46:37 -0300

    Web Service Key

--- src/services/GetWeatherDataService.ts
+++ src/services/GetWeatherDataService.ts
@@ -8,8 +8,8 @@ interface IRequest{
 class GetWeatherDataService {
   async execute({ cidade }: IRequest) {
     try {
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?appid=&q=${cidade}&units=metric`)
-
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?appid={chave}&q=${cidade}&units=metric`)
+      // https://openweathermap.org/
       return response.data;
     } catch (error) {
       throw new Error("Registro não encontrado!");
